92878eac7337f0e238e6de5ee28c2ee06bec921d
parkjunsik <akfldkskgorn@gmail.com>
2023-02-05 04:15:17 +0900

    문제풀이 : A+B(1000번)

---
+++ baekjoon/src/Baekjoon_1000.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1000 {
+  public void scan() throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력받기
+    String str = br.readLine(); // 한줄씩 받기
+    StringTokenizer st = new StringTokenizer(str); // " "를 기준으로 나눈기
+
+    System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+    br.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    new Baekjoon_1000().scan();
+  }
+}
+
