d94358d42db00040170845ff03da9895a7f86345
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-08 07:10:22 +0900

    코드수정 : 비밀번호 찾기(17219번)
    //시간초과 - 해결
    - Util의 HashMap을 사용하여 시간초과를 해결하였다.

--- baekjoon/src/hashtable/Baekjoon_17219.java
+++ baekjoon/src/hashtable/Baekjoon_17219.java
@@ -3,51 +3,10 @@ package hashtable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Baekjoon_17219 {
-  static class HashTable {
-    Data[] dataList;
-
-    HashTable(int max) {
-      dataList = new Data[max];
-    }
-
-    static class Data {
-      String key;
-      String value;
-
-      Data(String key, String value) {
-        this.key = key;
-        this.value = value;
-      }
-    }
-
-    public int getHash(String key) {
-      int hash = 0;
-      for (var i = 0; i < key.length(); i++) {
-        hash = (hash + key.charAt(i) * (i + 1)) % dataList.length;
-      }
-      return hash;
-    }
-
-    public void push(String key, String value) {
-      int hash = getHash(key);
-      while (dataList[hash] != null && !dataList[hash].key.equals(key)) {
-        hash = (hash + 1) % dataList.length;
-      }
-      dataList[hash] = new Data(key, value);
-    }
-
-    public String get(String key) {
-      int hash = getHash(key);
-      while (dataList[hash] != null && !dataList[hash].key.equals(key)) {
-        hash = (hash + 1) % dataList.length;
-      }
-      return dataList[hash] != null ? dataList[hash].value : null;
-    }
-  }
-
   public static void result() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력받기
     StringBuilder sb = new StringBuilder();
@@ -58,12 +17,12 @@ public class Baekjoon_17219 {
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
 
-    HashTable hashTable = new HashTable(n);
+    HashMap<String, String> hashTable = new HashMap<>(n);
 
     for (var i = 0; i < n; i++) {
       str = br.readLine();
       st = new StringTokenizer(str);
-      hashTable.push(st.nextToken(), st.nextToken());
+      hashTable.put(st.nextToken(), st.nextToken());
     }
     for (var i = 0; i < m; i++) {
       str = br.readLine();
@@ -72,7 +31,6 @@ public class Baekjoon_17219 {
     }
     System.out.println(sb);
   }
-
   public static void main(String[] args) throws Exception {
     result();
   }
