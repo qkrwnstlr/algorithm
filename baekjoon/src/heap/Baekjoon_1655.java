3626583acabae0f7adc1148318107cf72caa0a92
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-13 16:45:04 +0900

    코드수정 : 가운데를 말해요(1655번)
    //틀렸습니다
    - 출력을 하나 들어올때 마다 했는데 그게 문제였던것 같다

--- baekjoon/src/heap/Baekjoon_1655.java
+++ baekjoon/src/heap/Baekjoon_1655.java
@@ -1,13 +1,12 @@
 package heap;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Baekjoon_1655 {
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
   static int median;
   static PriorityQueue<Integer> over = new PriorityQueue<>(Comparator.comparingInt(i -> i)); // 더 큰 값들을 작은 순서대로 정렬
   static PriorityQueue<Integer> low = new PriorityQueue<>((i1, i2) -> i2 - i1); // 더 작은 값들을 큰 순서대로 정렬
@@ -19,20 +18,24 @@ public class Baekjoon_1655 {
   static void result() throws IOException {
     int n = input();
     median = input();
-    System.out.println(median);
+    bw.write(String.valueOf(median));
+    bw.write('\n');
+
     for (int i = 1; i < n; i++) {
       int input = input();
       if (input > median) over.add(input);
       else low.add(input);
-      if ((n + 1) % 2 == 0 && over.size() - low.size() > 1) { // 지금까지 들어온거의 갯수가 짝수면 over.size가 1더 커야함
-        low.add(median);
-        if (!over.isEmpty()) median = over.poll();
-      } else if (low.size() - over.size() > 0) { // 홀수면 같아야함
+      if(low.size() > over.size()) {
         over.add(median);
-        if (!low.isEmpty()) median = low.poll();
+        median = low.poll();
+      } else if(over.size() > low.size() + 1) {
+        low.add(median);
+        median = over.poll();
       }
-      System.out.println(median);
+      bw.write(String.valueOf(median));
+      bw.write('\n');
     }
+    bw.flush();
   }
 
   public static void main(String[] args) throws IOException {
