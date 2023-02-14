a513908b524beedb72f12506ade62a11680d460d
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-14 13:16:56 +0900

    코드수정 : 이항 계수 2(11051번)
    //런타임 에러 (ArrayIndexOutOfBounds)
    - k를 담당하는 배열의 길이를 늘려 해결하였다.

--- baekjoon/src/math/Baekjoon_11051.java
+++ baekjoon/src/math/Baekjoon_11051.java
@@ -13,22 +13,23 @@ public class Baekjoon_11051 {
     int n = Integer.parseInt(st.nextToken());
     int k = Integer.parseInt(st.nextToken());
 
-    if(k == 0) {
-      System.out.println(1);
-      return;
-    }
-
-    int[][] cash = new int[1000][1000];
-    for (int i = 0; i < n; i++) {
-      cash[i][i] = 1;
-      cash[i][0] = i + 1;
+    int[][] cash = new int[n][k + 1]; // (1 ~ n) C (0 ~ k)
+    for (int i = 0; i < n; i++) { // 초기값 설정
+      cash[i][0] = 1; // nC0 = 1
+      if(k == 0) continue;
+      cash[i][1] = i + 1; // nC1 = n
+      if(i >= k) continue;
+      cash[i][i] = i + 1; // nCn-1 = n
+      cash[i][i + 1] = 1; // nCn = 1
     }
     for (int i = 1; i < n; i++) {
-      for (int j = 1; j < k; j++) {
-        if (cash[i][j] == 0 && i > j) cash[i][j] = (cash[i - 1][j - 1] + cash[i - 1][j]) % 10007;
+      for (int j = 2; j <= k; j++) {
+        if (cash[i][j] == 0 && i >= j) { // 아직 값을 구하지 않았으면 계산, nCk에서 k가 더 크면 0이므로 계산하지 마라
+          cash[i][j] = (cash[i - 1][j - 1] + cash[i - 1][j]) % 10007; // nCk = n-1Ck-1 + n-1Ck
+        }
       }
     }
-    System.out.println(cash[n - 1][k - 1]);
+    System.out.println(cash[n - 1][k]); // nCk
   }
 
   public static void main(String[] args) throws IOException {
