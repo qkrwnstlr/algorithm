99759603f3f73afc558a8560f8440b6fca58819c
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-15 23:24:45 +0900

    코드수정 : 제곱ㄴㄴ수(1016번)
    //시간 초과
    - 역발상으로 소수의 제곱의 배수인 수들이 제곱ㄴㄴ수가 아니라 판단하고 이를 구한후 전체에서 빼는 방법으로 해결하였다.

--- baekjoon/src/math/Baekjoon_1016.java
+++ baekjoon/src/math/Baekjoon_1016.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Baekjoon_1016 {
@@ -12,9 +13,8 @@ public class Baekjoon_1016 {
   static long max;
   static boolean[] isSquare;
   static boolean[] isPrime;
-  static ArrayList<Long> squareList = new ArrayList<>();
+  static HashSet<Long> squareSet = new HashSet<>();
   static ArrayList<Integer> primeList = new ArrayList<>();
-  static boolean[] isChecked;
 
   static void eratosthenes() {
     int sqrtMax = (int) Math.sqrt(max);
@@ -38,27 +38,15 @@ public class Baekjoon_1016 {
     Arrays.fill(isSquare, false);
     Arrays.fill(isPrime, true);
     eratosthenes(); // 소수만 고름
-    isChecked = new boolean[primeList.size()];
-    Arrays.fill(isChecked, false);
-    for (int i = 0; i < max - min + 1; i++) {
-      if (isSquare[i]) continue;
-      for (int p = 0; p < primeList.size(); p++) {
-        if(isChecked[p]) continue; // 이미 체크한 소수는 넘어감
-        int j = primeList.get(p); // 앞으로 체크할 소수
-        if ((long) j * j > i + min) break; // 만약 소수의 제곱이 나머지를 확인할 값보다 크면 절데 0이 아니므로 넘어감
-        if ((i + min) % ((long) j * j) == 0) { // 만약 제곱수로 나누어 떨어지면(제곰 ㄴㄴ수가 아니라면)
-          isChecked[p] = true; // 해당 소수의 제곱에 걸리는 수는 이미 모두 체크함을 알림
-          for (long k = i + min; k <= max; k += (long) j * j) { // 해당 수와 같은 소수의 제곱으로 나누어 떨어지는 모든 값을 제곱수가 아니라고 판단함
-            int index = (int) (k - min);
-            if(isSquare[index]) continue; // 36 같이 4에도 걸리고 9에도 걸리는 값이 중복되는 것을 막음
-            squareList.add(k);
-            isSquare[index] = true; // 제곱수로 나누어 떨어짐을 저장
-          }
-          break;
-        }
+
+    for (int i : primeList) { // 소수들에 대해서
+      long square = (long) i * i;
+      for (long j = (min / square) * square; j <= max; j += square) { // 소수의 제곱의 배수가
+        if (j >= min) squareSet.add(j); // min과 max사이면 제곱 ㄴㄴ수가 아니다.
       }
     }
-    System.out.println(max - min + 1 - squareList.size());
+
+    System.out.println(max - min + 1 - squareSet.size());
   }
 
   public static void main(String[] args) throws IOException {
