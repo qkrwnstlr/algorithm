67672704eeceee017b5f79b74d7ed4fba6c88e72
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-21 15:55:50 +0900

    코드수정 : 재귀 문자열(23564번)
    //틀렸습니다
    - 각 문자가 몇개가 포함되어있는지로 반복 횟수를 계산하였으나 aba(111) 같은 같은 문자가 다른 순서로 추가되는 경우를 고려하지 않아서 문제가 생겼었다. 각 문자마다 새로운 문자인지 기존 문자인지 확인하여 겹치는 문자더라도 따로 추가될수 있게 변경하였다.
    
    //시간초과
    - 중복된 계산이 너무 많아서 생긴 문제인것 같다. 문자열의 길이를 저장해 놓으면 조금은 줄것 같다.

--- baekjoon/src/recursion/Baekjoon_23564.java
+++ baekjoon/src/recursion/Baekjoon_23564.java
@@ -2,39 +2,57 @@ package recursion;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Baekjoon_23564 {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static String str;
+
+  static char stringPop() {
+    char result = str.charAt(0);
+    str = str.substring(1);
+    return result;
+  }
+
   static void result() throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    String str = br.readLine();
+    str = br.readLine();
+
     ArrayList<Character> characters = new ArrayList<>();
     ArrayList<Integer> counts = new ArrayList<>();
-    HashMap<Character, Integer> indexMap = new HashMap<>();
-    int index = 0;
-    for (int i = 0; i < str.length(); i++) {
-      char current = str.charAt(i);
-      if (!indexMap.containsKey(current)) {
-        indexMap.put(current, index++);
+
+    char current = stringPop();
+    characters.add(current);
+    counts.add(1);
+
+    // 같은 순번에 들어온 문자열이 연속으로 나오는 경우는 처음 밖에 없다.
+    // 즉, 처음을 제외하면 한번 문자가 바뀌면 그 뒤에는 반복되었던 문자가 나온다.
+    // 이를 제거하면 직전에 추가되었던 문자 혹은 새로운 문자가 나온다.
+    while (true) {
+      int before = 0;
+      int temp = 1;
+      int accLength = 0; // 반복되는 문자열의 길이
+      for (int i = counts.size() - 2; i >= 0; i--) { // 반복되는 문자열의 길이를 구한다
+        temp *= before + 1;
+        accLength += counts.get(i) * temp;
+        before = counts.get(i);
+      }
+
+      for (int i = 0; i < accLength; i++) stringPop(); // 반복되는 문자열을 제거한다
+
+      if (str.isEmpty()) break;
+
+      current = stringPop(); // 다음 문자열을 확인한다
+
+      if (current == characters.get(characters.size() - 1)) { // 만약 직전에 나온거와 같으면
+        counts.set(counts.size() - 1, counts.get(counts.size() - 1) + 1); // 그 수를 늘린다
+      } else { // 다르다면
+        characters.add(current); // 새로운 문자를 추가한다
         counts.add(1);
-        characters.add(current);
-      } else {
-        counts.set(indexMap.get(current), counts.get(indexMap.get(current)) + 1);
       }
     }
-
-    int current = 0;
-    int factorial = 1;
-    for (int i = counts.size() - 1; i >= 0; i--) {
-      factorial *= current + 1;
-      current = counts.get(i) / factorial;
-      counts.set(i, current);
-    }
-
     characters.forEach(e -> {
       try {
-        bw.write(e.toString());
+        bw.write(e);
       } catch (IOException ex) {
         throw new RuntimeException(ex);
       }
