e6147d364d21ea8cd1039e19e6115c75d5e94f2c
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-22 14:43:01 +0900

    코드수정 : 정렬 게임(13415번)
    //시간초과
    - 정렬할 필요가 있을 때만 정렬을 하는 방식으로 수정했으나 최악의 경우에는 여전히 같기에 여전히 시간초과가 발생했다.

--- baekjoon/src/sort/Baekjoon_13415.java
+++ baekjoon/src/sort/Baekjoon_13415.java
@@ -8,6 +8,26 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Baekjoon_13415 {
+  static class ArrayData implements Comparable<ArrayData> {
+    int value;
+    int index;
+    char location;
+
+    ArrayData(int value, int index, char location) {
+      this.value = value;
+      this.index = index;
+      this.location = location;
+    }
+
+    @Override
+    public int compareTo(ArrayData o) {
+      return this.value - o.value == 0 ?
+              this.index - o.index == 0 ?
+                      o.location - this.location : this.index - o.index
+              : this.value - o.value;
+    }
+  }
+
   static void result() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
@@ -17,12 +37,27 @@ public class Baekjoon_13415 {
     StringTokenizer st = new StringTokenizer(str);
     for (int i = 0; i < n; i++) ints[i] = Integer.parseInt(st.nextToken());
     int k = Integer.parseInt(br.readLine());
-    for (int i = 0; i < k; i++) {
-      str = br.readLine();
-      st = new StringTokenizer(str);
-      Arrays.sort(ints, 0, Integer.parseInt(st.nextToken()));
-      Arrays.sort(ints, 0, Integer.parseInt(st.nextToken()), Comparator.reverseOrder());
+
+    ArrayData[] list = new ArrayData[k * 2];
+
+    for (int i = 0; i < k * 2; i += 2) {
+      st = new StringTokenizer(br.readLine());
+      list[i] = new ArrayData(Integer.parseInt(st.nextToken()), i / 2, 'a');
+      list[i + 1] = new ArrayData(Integer.parseInt(st.nextToken()), i / 2, 'b');
     }
+
+    Arrays.sort(list, Comparator.reverseOrder());
+
+    int maxIndex = -1;
+    for (int i = 0; i < k * 2; i++) {
+      if (list[i].index >= maxIndex) {
+        if(list[i].location == 'a' && list[i].index == maxIndex) continue;
+        maxIndex = list[i].index;
+        if (list[i].location == 'a') Arrays.sort(ints, 0, list[i].value);
+        else Arrays.sort(ints, 0, list[i].value, Comparator.reverseOrder());
+      }
+    }
+
     for (int i = 0; i < n; i++) sb.append(ints[i]).append(" ");
     System.out.println(sb);
   }
