73986130fb06485d93f88fccb0d9b65739a87e47
qkrwnstlr <akfldkskgorn@gmail.com>
2023-03-02 14:52:03 +0900

    코드수정 : 숨바꼭질(1697번)
    //틀렸습니다
    - 기존 알고리즘은 소인수 분해의 결과가 값이 큰 소수라면 정상적인 값을 서치할수 없었다. 따라서 BFS로 구현하였다.

--- baekjoon/src/search/Baekjoon_1697.java
+++ baekjoon/src/search/Baekjoon_1697.java
@@ -3,20 +3,32 @@ package search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baekjoon_1697 {
   static int n;
   static int k;
 
-  public static int move(int current, int i) {
-    if (current / 2 + 1 < n) return i + Math.abs(current - n);
-    if (current == n) return i;
-    if (current % 2 != 0) {
-      int a = move(current + 1, i + 1);
-      int b = move(current - 1, i + 1);
-      return Math.min(a, b);
-    } else return move(current / 2, i + 1);
+  static Queue<Integer> queue = new LinkedList<>();
+  static int[] visited = new int[100001];
+
+  static void setVisited(int current, int i) {
+    queue.add(current);
+    visited[current] = i;
+  }
+
+  static int BFS() {
+    setVisited(n, 0);
+    while (!queue.isEmpty()) {
+      int current = queue.remove();
+      if (current == k) return visited[current];
+      if (current - 1 >= 0 && visited[current - 1] == 0) setVisited(current - 1, visited[current] + 1);
+      if (current + 1 <= 100000 && visited[current + 1] == 0) setVisited(current + 1, visited[current] + 1);
+      if (current * 2 <= 100000 && visited[current * 2] == 0) setVisited(current * 2, visited[current] + 1);
+    }
+    return Math.abs(n - k);
   }
 
   public static void result() throws IOException {
@@ -25,7 +37,7 @@ public class Baekjoon_1697 {
     StringTokenizer st = new StringTokenizer(str);
     n = Integer.parseInt(st.nextToken());
     k = Integer.parseInt(st.nextToken());
-    System.out.println(move(k, 0));
+    System.out.println(BFS());
   }
 
   public static void main(String[] args) throws IOException {
