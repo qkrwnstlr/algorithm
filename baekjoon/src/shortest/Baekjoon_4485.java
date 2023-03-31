262e52d1db39d2e6826e8ba4e29c9f1bf36572e3
qkrwnstlr <akfldkskgorn@gmail.com>
2023-03-31 13:25:52 +0900

    문제풀이 : 녹색 옷 입은 애가 젤다지?(4485번)

--- baekjoon/src/shortest/Baekjoon_4485.java
+++ baekjoon/src/shortest/Baekjoon_4485.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -30,10 +31,11 @@ public class Baekjoon_4485 {
   static int[] table;
   static ArrayList<ArrayList<Node>> graph;
 
-  static int[][] d;
+  static int[] d;
 
   static void initGraph() {
     for (int i = 0; i < n * n; i++) {
+      graph.add(new ArrayList<>());
       if (i % n != 0) graph.get(i).add(new Node(i - 1, table[i - 1]));
       if (i / n != 0) graph.get(i).add(new Node(i - n, table[i - n]));
       if (i % n != n - 1) graph.get(i).add(new Node(i + 1, table[i + 1]));
@@ -41,12 +43,13 @@ public class Baekjoon_4485 {
     }
   }
 
-  static void dijkstra(int start) {
+  static int dijkstra(int end) {
     PriorityQueue<Node> pq = new PriorityQueue<>();
     /* 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입 */
-    pq.offer(new Node(start, 0));
-
-    d[start][start] = 0;
+    pq.offer(new Node(0, 0));
+    d = new int[n * n];
+    Arrays.fill(d, Integer.MAX_VALUE);
+    d[0] = 0;
 
     /* 큐가 비어있지않을 때까지 반복 */
     while (!pq.isEmpty()) {
@@ -57,22 +60,23 @@ public class Baekjoon_4485 {
       int now = node.index; // 현재 노드 번호
 
       /* 현재 노드가 이미 처리된 적이 있는 노드라면 무시 */
-      if (d[start][now] < dist) {
+      if (d[now] < dist) {
         continue;
       }
 
       /* 현재 노드와 연결된 다른 인접한 노드들을 확인 */
       for (int i = 0; i < graph.get(now).size(); i++) {
         /* 현재의 최단거리 + 현재의 연결된 노드의 비용 */
-        int cost = d[start][now] + graph.get(now).get(i).distance;
+        int cost = d[now] + graph.get(now).get(i).distance;
 
         /* 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우 */
-        if (cost < d[start][graph.get(now).get(i).index]) {
-          d[start][graph.get(now).get(i).index] = cost;
+        if (cost < d[graph.get(now).get(i).index]) {
+          d[graph.get(now).get(i).index] = cost;
           pq.offer(new Node(graph.get(now).get(i).index, cost));
         }
       }
     }
+    return d[end];
   }
 
   static void result() throws IOException {
@@ -86,13 +90,11 @@ public class Baekjoon_4485 {
       graph = new ArrayList<>();
       for (int i = 0; i < n; i++) {
         str = br.readLine();
-        graph.add(new ArrayList<>());
         StringTokenizer st = new StringTokenizer(str);
         for (int j = 0; j < n; j++) table[i * n + j] = Integer.parseInt(st.nextToken());
       }
       initGraph();
-      // TODO : 최단거리 구하기
-      System.out.println("Problem " + (count++) + ": ");
+      System.out.println("Problem " + (count++) + ": " + (dijkstra(n * n - 1) + table[0]));
     }
   }
 
