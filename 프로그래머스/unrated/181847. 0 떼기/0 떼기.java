3019ab88e145beb6e95c1737f1099c3501cb69da
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-07-12 09:43:07 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        for(int i = 0; i < n_str.length(); i++) {
+            if(n_str.charAt(i) != '0') return n_str.substring(i);
+        }
+        return answer;
+    }
+}
