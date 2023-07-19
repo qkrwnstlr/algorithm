3b47b8954b1ed27e2e60140ca4e1b62a4b578e6f
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-07-19 21:03:51 +0900

    [unrated] Title: 그림 확대, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        List<String> answerList = new ArrayList<>();
+        for (String row : picture) {
+            StringBuilder enlargedRow = new StringBuilder();
+            for (char pixel : row.toCharArray()) {
+                for (int i = 0; i < k; ++i) {
+                    enlargedRow.append(pixel);
+                }
+            }
+            for (int i = 0; i < k; ++i) {
+                answerList.add(enlargedRow.toString());
+            }
+        }
+        String[] answer = new String[answerList.size()];
+        answer = answerList.toArray(answer);
+        return answer;
+    }
+}
