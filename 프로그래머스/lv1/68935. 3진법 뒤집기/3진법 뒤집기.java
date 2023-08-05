79d862695c29a09a36628a34b4325e1a0766ea71
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-08-05 21:40:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -8,8 +8,6 @@ class Solution {
             sb.append(n % 3);
             n /= 3;
         }
-        String[] temp = sb.reverse().toString().split("");
-        System.out.println(Arrays.toString(temp));
-        return IntStream.range(0, temp.length).map(i -> (int) Math.pow(3, i) * Integer.parseInt(temp[i])).sum();
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
