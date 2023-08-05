f45094c6c1f88ab9db76b12ea48e052c6193fae8
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-08-05 21:05:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.15 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        return s.length() == 4 || s.length() == 6 ? s.replaceAll("[0-9]", "").length() == 0 ? true : false : false;
+        return s.matches("\\d{4}|\\d{6}");
     }
 }
