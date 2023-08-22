b6a283ed9e37363174035307a8279a25f7c422ca
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-08-23 05:59:09 +0900

    [Silver III] Title: N과 M (3), Time: 828 ms, Memory: 185400 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+  static int N;
+  static int M;
+  static StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    M = sc.nextInt();
+    dfs(0, new ArrayList<>());
+    System.out.println(sb);
+  }
+
+  static void dfs(int depth, ArrayList<Integer> list) {
+    if(depth == M) {
+      for(int i : list) sb.append(i).append(" ");
+      sb.append("\n");
+      return;
+    }
+    for(int i = 1; i <= N; i++) {
+      ArrayList<Integer> temp = (ArrayList<Integer>) list.clone();
+      temp.add(i);
+      dfs(depth + 1, temp);
+    }
+  }
+}
