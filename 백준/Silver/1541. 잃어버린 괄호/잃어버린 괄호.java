160465a2287049da67d7626bff7eed01bf68432e
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-09-27 17:15:22 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String formula = br.readLine();
+    String[] split = formula.split("");
+
+    Queue<Integer> integers = new LinkedList<>();
+    Queue<String> operators = new LinkedList<>();
+
+    int temp = 0;
+
+    for (String s : split) {
+      try {
+        temp = temp * 10 + Integer.parseInt(s);
+      } catch (Exception e) {
+        operators.add(s);
+        integers.add(temp);
+        temp = 0;
+      }
+    }
+
+    integers.add(temp);
+
+    int result = integers.poll();
+    String lastOperator = "";
+    temp = 0;
+
+    while (!integers.isEmpty()) {
+      String operator = operators.poll();
+      if(operator.equals("-")) {
+        if(lastOperator.equals("-")) result -= temp;
+        else result += temp;
+        lastOperator = "-";
+        temp = integers.poll();
+      } else {
+        if(lastOperator.equals("-")) temp += integers.poll();
+        else {
+          lastOperator = "+";
+          result += integers.poll();
+        }
+      }
+    }
+
+    if(lastOperator.equals("-")) result -= temp;
+    else result += temp;
+
+    System.out.println(result);
+  }
+}
