3d4c388d3c1c4d5f0bb999482d13babab325fe1b
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-09-27 12:04:42 +0900

    [Silver III] Title: 주유소, Time: 376 ms, Memory: 36144 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,17 +5,17 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
-    int[] distance = new int[N - 1];
-    int[] price = new int[N];
+    long[] distance = new long[N - 1];
+    long[] price = new long[N];
     StringTokenizer st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N - 1; i++) distance[i] = Integer.parseInt(st.nextToken());
+    for (int i = 0; i < N - 1; i++) distance[i] = Long.parseUnsignedLong(st.nextToken());
     st = new StringTokenizer(br.readLine());
-    int min = Integer.MAX_VALUE;
+    long min = Integer.MAX_VALUE;
     for (int i = 0; i < N; i++) {
-      min = Math.min(min, Integer.parseInt(st.nextToken()));
+      min = Math.min(min, Long.parseUnsignedLong(st.nextToken()));
       price[i] = min;
     }
-    int total = 0;
+    long total = 0;
     for(int i = 0; i < N - 1; i++) total += distance[i] * price[i];
     System.out.println(total);
   }
