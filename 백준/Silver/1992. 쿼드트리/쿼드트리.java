7bf177c7993ae0c7b057fe140f4457026c02eda8
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-10-02 20:47:59 +0900

    [Silver I] Title: 쿼드트리, Time: 160 ms, Memory: 15552 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  static int[][] paper;
+  static int N;
+
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    paper = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      String[] temp = br.readLine().split("");
+      for (int j = 0; j < N; j++) paper[i][j] = Integer.parseInt(temp[j]);
+    }
+
+    slice(0, 0, N);
+
+    String temp = sb.toString();
+    temp = temp.replace("(0)", "0");
+    temp = temp.replace("(1)", "1");
+    System.out.println(temp);
+  }
+
+  static void slice(int x, int y, int size) {
+    sb.append("(");
+    int data = paper[x][y];
+    for (int i = x; i < x + size; i++) {
+      for (int j = y; j < y + size; j++) {
+        if (data == paper[i][j]) continue;
+        slice(x, y, size / 2);
+        slice(x, y + size / 2, size / 2);
+        slice(x + size / 2, y, size / 2);
+        slice(x + size / 2, y + size / 2, size / 2);
+        sb.append(")");
+        return;
+      }
+    }
+    sb.append(data);
+    sb.append(")");
+  }
+}
