2882c89462f1cca851854c700e2852508f954723
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2023-10-03 16:45:02 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int A, B, C;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    A = Integer.parseInt(st.nextToken());
+    B = Integer.parseInt(st.nextToken());
+    C = Integer.parseInt(st.nextToken());
+
+    System.out.println(calculate(A, B));
+  }
+
+  static long calculate(int a, int b) {
+    if (b == 1) return a % C;
+    long temp = calculate(a, b / 2);
+    if (b % 2 == 0) return temp * temp % C;
+    return (temp * temp % C) * (a % C) % C;
+  }
+}
