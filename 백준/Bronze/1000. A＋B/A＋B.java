452a36f168996fb4cbb3f86d9cf81d6124f4aaf0
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2024-02-21 15:21:38 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public void result() throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력받기
+    String str = br.readLine(); // 한줄씩 받기
+    StringTokenizer st = new StringTokenizer(str); // " "를 기준으로 나눈기
+
+    System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+    br.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    new Main().result();
+  }
+}
