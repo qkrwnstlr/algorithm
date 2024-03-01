7f6710ffc698b2b1cdb10006fa18cebfb2c9e033
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2024-03-01 22:24:38 +0900

    2024.03.01

--- swea/특이한_자석.java
+++ swea/특이한_자석.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Solution {
   public static void main(String[] args) throws IOException {
-    System.setIn(new FileInputStream("res/input.txt"));
     new Solution().run();
   }
 
