408e42273db8529064af158be188f3a379b84d8c
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2024-03-01 22:07:57 +0900

    2024.03.01

---
+++ swea/특이한_자석.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    new Solution().run();
+  }
+
+  BufferedReader br;
+  StringBuilder sb;
+  int N, K, result;
+  int[][] magnets;
+  int[] top;
+  int[][] rotations;
+
+  void init() throws IOException {
+    N = 4;
+    K = Integer.parseInt(br.readLine());
+    result = 0;
+    magnets = new int[N + 1][8];
+    top = new int[N + 1];
+    for (int i = 1; i <= N; i++) {
+      magnets[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+    rotations = new int[K][2];
+    for (int i = 0; i < K; i++) {
+      rotations[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+  }
+
+  void run() throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    sb = new StringBuilder();
+
+    int T = Integer.parseInt(br.readLine());
+    for (int testCase = 1; testCase <= T; testCase++) {
+      init();
+      solution();
+      sb.append("#").append(testCase).append(" ").append(result).append("\n");
+    }
+
+    System.out.println(sb);
+
+    br.close();
+  }
+
+  void solution() {
+    for (int i = 0; i < K; i++) {
+      int target = rotations[i][0];
+      int direction = rotations[i][1];
+      rotate(target, direction);
+    }
+    for (int j = 1; j <= N; j++) {
+      if (magnets[j][top[j]] == 1) {
+        result += (2 << j - 1) / 2;
+      }
+    }
+  }
+
+  void rotate(int target, int direction) {
+    int[] distances = new int[N + 1];
+    distances[target] = -direction;
+
+    for (int current = target; current > 1 && magnets[current][movedIndex(current, -2)] != magnets[current - 1][movedIndex(current - 1, 2)]; current--) {
+      distances[current - 1] = -distances[current];
+    }
+
+    for (int current = target; current < N && magnets[current][movedIndex(current, 2)] != magnets[current + 1][movedIndex(current + 1, -2)]; current++) {
+      distances[current + 1] = -distances[current];
+    }
+
+    for (int i = 1; i <= N; i++) {
+      top[i] = movedIndex(i, distances[i]);
+    }
+  }
+
+  int movedIndex(int target, int distance) {
+    return (8 + top[target] + distance) % 8;
+  }
+}
