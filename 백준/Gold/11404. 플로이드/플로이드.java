0a03fb9a2d55bb1b537741674a4b340630abebe7
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2024-03-27 15:56:01 +0900

    [Gold IV] Title: 플로이드, Time: 432 ms, Memory: 43164 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,52 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-  static int N, M;
-  static long[][] distance;
+	public static void main(String[] args) throws IOException {
+		new Main().run();
+	}
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
+	int N, M;
+	long[][] graph;
 
-    N = Integer.parseInt(br.readLine());
-    M = Integer.parseInt(br.readLine());
+	void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		graph = new long[N][N];
 
-    distance = new long[N + 1][N + 1];
-    for (int i = 1; i <= N; i++) {
-      Arrays.fill(distance[i], Long.MAX_VALUE);
-      distance[i][i] = 0;
-    }
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(graph[i], Integer.MAX_VALUE);
+			graph[i][i] = 0;
+		}
 
-    for (int i = 0; i < M; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int A = Integer.parseInt(st.nextToken());
-      int B = Integer.parseInt(st.nextToken());
-      long C = Long.parseLong(st.nextToken());
-      distance[A][B] = Long.min(distance[A][B], C);
-    }
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken()) - 1;
+			int v = Integer.parseInt(st.nextToken()) - 1;
+			int w = Integer.parseInt(st.nextToken());
+			graph[u][v] = Math.min(graph[u][v], w);
+		}
+	}
 
-    FloydWarshall();
+	void run() throws IOException {
+		init();
+		solution();
+		StringBuffer sb = new StringBuffer();
+		Arrays.stream(graph).forEach(arr -> {
+			Arrays.stream(arr).forEach(i -> sb.append(i == Integer.MAX_VALUE ? 0 : i).append(" "));
+			sb.append("\n");
+		});
+		System.out.println(sb);
+	}
 
-    for (int i = 1; i <= N; i++) {
-      for (int j = 1; j <= N; j++) sb.append(distance[i][j] == Long.MAX_VALUE ? 0 : distance[i][j]).append(" ");
-      sb.append("\n");
-    }
-
-    System.out.println(sb);
-  }
-
-  static void FloydWarshall() {
-    for (int i = 1; i <= N; i++) {
-      for (int j = 1; j <= N; j++) {
-        if (distance[j][i] == Long.MAX_VALUE) continue;
-        for (int k = 1; k <= N; k++) {
-          if (distance[i][k] == Long.MAX_VALUE) continue;
-          distance[j][k] = Long.min(distance[j][i] + distance[i][k], distance[j][k]);
-        }
-      }
-    }
-  }
+	void solution() {
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+				}
+			}
+		}
+	}
 }
