3f532386fc90e728db6d76aa6581605b0ef4de86
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2024-04-19 02:57:21 +0900

    [Gold I] Title: 기계오리 연구, Time: 280 ms, Memory: 17336 KB -BaekjoonHub

---
+++ 백준/Gold/28082. 기계오리 연구/기계오리 연구.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    new Main().run();
+  }
+
+  int N, K, result, M;
+  int[] I, dp;
+
+  void init() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    result = 0;
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+    M = K * 500 + 2;
+    I = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    dp = new int[M];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+  }
+
+  void run() throws IOException {
+    init();
+    solution();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i < M; i++) {
+      if (dp[i] == Integer.MAX_VALUE) continue;
+      result++;
+      sb.append(i).append(" ");
+    }
+    System.out.println(result);
+    System.out.println(sb);
+  }
+
+  void solution() {
+    for (int i = 0; i < N; i++) {
+      for (int j = M - 1; j > 0; j--) {
+        if (j + I[i] < M && dp[j] < K) {
+          dp[j + I[i]] = Math.min(dp[j] + 1, dp[j + I[i]]);
+        }
+      }
+      dp[I[i]] = 1;
+    }
+  }
+}
