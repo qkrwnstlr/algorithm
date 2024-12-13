384793d4361e3f39527f890ec5ad5d8ff4a2b574
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2024-12-13 09:32:38 +0900

    [Silver II] Title: 종이의 개수, Time: 952 ms, Memory: 319728 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,53 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-  static int[][] paper;
-  static int N;
-  static int zero = 0;
-  static int plus = 0;
-  static int minus = 0;
-
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    N = Integer.parseInt(br.readLine());
-    paper = new int[N][N];
-
-    for (int i = 0; i < N; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < N; j++) paper[i][j] = Integer.parseInt(st.nextToken());
-    }
-
-    slice(0, 0, N);
-
-    System.out.println(minus);
-    System.out.println(zero);
-    System.out.println(plus);
+    new Main().run();
   }
 
-  static void slice(int x, int y, int size) {
-    int color = paper[x][y];
-    for (int i = x; i < x + size; i++) {
-      for (int j = y; j < y + size; j++) {
-        if (color == paper[i][j]) continue;
-        slice(x, y, size / 3);
-        slice(x, y + size / 3, size / 3);
-        slice(x, y + size * 2 / 3, size / 3);
-        slice(x + size / 3, y, size / 3);
-        slice(x + size / 3, y + size / 3, size / 3);
-        slice(x + size / 3, y + size * 2 / 3, size / 3);
-        slice(x + size * 2 / 3, y, size / 3);
-        slice(x + size * 2 / 3, y + size / 3, size / 3);
-        slice(x + size * 2 / 3, y + size * 2 / 3, size / 3);
+  BufferedReader br;
+  StringBuilder sb;
+  int N;
+  int[] result;
+  int[][] paper;
+
+  void init() throws IOException {
+    result = new int[3];
+    N = Integer.parseInt(br.readLine());
+    paper = new int[N][N];
+    for (int x = 0; x < N; x++)
+      paper[x] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+  }
+
+  void run() throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    sb = new StringBuilder();
+
+    init();
+    solution();
+    for (int i = 0; i < 3; i++) sb.append(result[i]).append("\n");
+
+    System.out.println(sb);
+
+    br.close();
+  }
+
+  void solution() {
+    draw(0, 0, N);
+  }
+
+  void draw(int sx, int sy, int n) {
+    for (int x = sx; x < sx + n; x++) {
+      for (int y = sy; y < sy + n; y++) {
+        if (paper[x][y] == paper[sx][sy]) continue;
+        int n3 = n / 3;
+        for (int dx = 0; dx < 3; dx++) {
+          for (int dy = 0; dy < 3; dy++) {
+            draw(sx + n3 * dx, sy + n3 * dy, n3);
+          }
+        }
         return;
       }
     }
-    switch (color) {
-      case -1: minus++; break;
-      case 0: zero++; break;
-      case 1: plus++; break;
-    }
+    result[paper[sx][sy] + 1]++;
   }
 }
