1e0c595ddcb7c0f58e6e9c78dcfabe86f97c9e45
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2025-02-18 17:49:57 +0900

    [Gold I] Title: 석판, Time: 316 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Gold/1691. 석판/석판.java
+++ 백준/Gold/1691. 석판/석판.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,7 +10,6 @@ public class Main {
 
   BufferedReader br;
   int W, H, N, result;
-  Plate[] plates;
   int[][] dp;
 
   void run() throws IOException {
@@ -30,15 +28,17 @@ public class Main {
     H = Integer.parseInt(st.nextToken());
     N = Integer.parseInt(br.readLine());
 
-    plates = new Plate[N];
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      plates[i] = new Plate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+    dp = new int[W + 1][H + 1];
+    for (int i = 1; i <= W; i++) {
+      for (int j = 1; j <= H; j++) {
+        dp[i][j] = i * j;
+      }
     }
 
-    dp = new int[W + 1][H + 1];
-    for (int i = 1; i <= W; i++) Arrays.fill(dp[i], -1);
-    dp[0][0] = 0;
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      dp[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 0;
+    }
   }
 
   void printResult() {
@@ -46,30 +46,12 @@ public class Main {
   }
 
   void solution() {
-    result = solve(W, H);
-  }
-
-  int solve(int w, int h) {
-    if (dp[w][h] != -1) return dp[w][h];
-
-    dp[w][h] = w * h;
-    for (int i = 0; i < N; i++) {
-      if (plates[i].w > w || plates[i].h > h) continue;
-
-      int nw = w - plates[i].w, nh = h - plates[i].h;
-      int wh = solve(nw, h) + solve(plates[i].w, nh), hw = solve(w, nh) + solve(nw, plates[i].h);
-      dp[w][h] = Math.min(dp[w][h], Math.min(wh, hw));
+    for (int w = 1; w <= W; w++) {
+      for (int h = 1; h <= H; h++) {
+        for (int cutW = 1; cutW <= w / 2; cutW++) dp[w][h] = Math.min(dp[w][h], dp[w - cutW][h] + dp[cutW][h]);
+        for (int cutH = 1; cutH <= h / 2; cutH++) dp[w][h] = Math.min(dp[w][h], dp[w][h - cutH] + dp[w][cutH]);
+      }
     }
-
-    return dp[w][h];
-  }
-}
-
-class Plate {
-  int w, h;
-
-  Plate(int w, int h) {
-    this.w = w;
-    this.h = h;
+    result = dp[W][H];
   }
 }
