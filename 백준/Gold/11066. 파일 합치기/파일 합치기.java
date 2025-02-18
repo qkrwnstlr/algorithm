2ca4d7a334e5ed5f1c7a9ccad47a1550e49d35da
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2025-02-18 21:22:17 +0900

    [Gold III] Title: 파일 합치기, Time: 388 ms, Memory: 33420 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,39 +1,63 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public void result() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    new Main().run();
+  }
 
-		int T = Integer.parseInt(br.readLine());
+  BufferedReader br;
+  int K;
+  int[][] dp, size;
+  StringBuilder result;
 
-		for (int test_case = 0; test_case < T; test_case++) {
-			int K = Integer.parseInt(br.readLine());
-			int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+  void run() throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    result = new StringBuilder();
 
-			
-			int[][] result = new int[K][K];
-			int[][] size  = new int[K][K];
-			for(int i = 0; i < K; i++) size[i][i] = arr[i];
-			
-			for(int i = 1; i < K; i++) {
-				for(int j = 0; j < K - i; j++) {
-					int k = j + i;
-					result[j][k] = Integer.MAX_VALUE;
-					for(int l = j; l < k; l++) {
-						size[j][k] = size[j][l] + size[l + 1][k];
-						result[j][k] = Integer.min(result[j][k], result[j][l] + result[l + 1][k] + size[j][k]);
-					}
-				}
-			}
-			
-			System.out.println(result[0][K - 1]);
-		}
+    int T = Integer.parseInt(br.readLine());
 
-		br.close();
-	}
+    for (int t = 0; t < T; t++) {
+      init();
+      solution();
+    }
+    printResult();
 
-	public static void main(String[] args) throws IOException {
-		new Main().result();
-	}
+    br.close();
+  }
+
+  void init() throws IOException {
+    K = Integer.parseInt(br.readLine());
+
+    dp = new int[K][K];
+
+    size = new int[K][K];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < K; i++) size[i][i] = Integer.parseInt(st.nextToken());
+    for (int i = 1; i < K; i++) {
+      for (int j = 0; i + j < K; j++) {
+        size[j][i + j] = size[j][i + j - 1] + size[j + 1][i + j] - size[j + 1][i + j - 1];
+      }
+    }
+  }
+
+  void printResult() {
+    System.out.println(result);
+  }
+
+  void solution() {
+    for (int i = 1; i < K; i++) {
+      for (int j = 0; i + j < K; j++) {
+        int k = i + j;
+        dp[j][k] = Integer.MAX_VALUE;
+        for (int l = j; l < k; l++) {
+          dp[j][k] = Integer.min(dp[j][k], dp[j][l] + dp[l + 1][k] + size[j][k]);
+        }
+      }
+    }
+    result.append(dp[0][K - 1]).append("\n");
+  }
 }
