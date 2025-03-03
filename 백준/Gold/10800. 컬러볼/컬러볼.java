c7918d4bc4728c954169032d0c470ab5acdd83b1
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2025-03-03 10:26:45 +0900

    [Gold II] Title: 컬러볼, Time: 608 ms, Memory: 76156 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.java
+++ 백준/Gold/10800. 컬러볼/컬러볼.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,7 +13,7 @@ public class Main {
 
   int N, total;
   int[] counts, result;
-  Ball[] balls;
+  List<List<Ball>> balls;
 
   void run() throws IOException {
     init();
@@ -23,13 +25,18 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     N = Integer.parseInt(br.readLine());
-    balls = new Ball[N];
+    balls = new ArrayList<>();
     counts = new int[N + 1];
     result = new int[N];
 
+    for (int i = 0; i <= 2000; i++) {
+      balls.add(new ArrayList<>());
+    }
+
     for (int i = 0; i < N; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
-      balls[i] = new Ball(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+      int color = Integer.parseInt(st.nextToken()), size = Integer.parseInt(st.nextToken());
+      balls.get(size).add(new Ball(i, color));
     }
 
     br.close();
@@ -42,38 +49,31 @@ public class Main {
   }
 
   void solution() {
-    Arrays.sort(balls);
-
-    for (int i = 0; i < N; ) {
-      Ball current = balls[i];
-
-      int j = 0;
-      while (i + j < N && current.size == balls[i + j].size) {
-        result[balls[i + j].index] = total - counts[balls[i + j].color];
-        j++;
+    for (int i = 1; i <= 2000; i++) {
+      List<Ball> list = balls.get(i);
+      for (int j = 0; j < list.size(); j++) {
+        result[list.get(j).index] = total - counts[list.get(j).color];
       }
 
-      for (int k = 0; k < j; k++) {
-        total += balls[i + k].size;
-        counts[balls[i + k].color] += balls[i + k].size;
+      for (int j = 0; j < list.size(); j++) {
+        counts[list.get(j).color] += i;
       }
 
-      i += j;
+      total += list.size() * i;
     }
   }
 }
 
 class Ball implements Comparable<Ball> {
-  int index, color, size;
+  int index, color;
 
-  Ball(int index, int color, int size) {
+  Ball(int index, int color) {
     this.index = index;
     this.color = color;
-    this.size = size;
   }
 
   @Override
   public int compareTo(Ball o) {
-    return size == o.size ? Integer.compare(color, o.color) : Integer.compare(size, o.size);
+    return Integer.compare(color, o.color);
   }
 }
