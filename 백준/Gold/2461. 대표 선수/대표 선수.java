3968f22eb01db879be1d57b55bdd024ee13a72d9
qkrwnstlr <93969681+qkrwnstlr@users.noreply.github.com>
2025-03-20 10:39:24 +0900

    [Gold II] Title: 대표 선수, Time: 1308 ms, Memory: 147060 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -6,51 +6,50 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		new Main().run();
-	}
+  public static void main(String[] args) throws IOException {
+    new Main().run();
+  }
 
-	BufferedReader br;
-	StringTokenizer st;
-	int N, M;
-	long result;
-	int[] indexes;
-	long[][] scores;
+  BufferedReader br;
+  StringTokenizer st;
+  int N, M, result;
+  int[] indexes;
+  int[][] scores;
 
-	void init() throws IOException {
-		result = Long.MAX_VALUE;
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		scores = new long[N][M];
-		for (int i = 0; i < N; i++) {
-			scores[i] = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).sorted().toArray();
-		}
-		indexes = new int[N];
-	}
+  void init() throws IOException {
+    result = Integer.MAX_VALUE;
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    scores = new int[N][M];
+    for (int i = 0; i < N; i++) {
+      scores[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+    }
+    indexes = new int[N];
+  }
 
-	void run() throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
+  void run() throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
 
-		init();
-		solution();
+    init();
+    solution();
 
-		System.out.println(result);
-	}
+    System.out.println(result);
+  }
 
-	void solution() {
-		PriorityQueue<Integer> pq = new PriorityQueue<>((c1, c2) -> scores[c1][indexes[c1]] - scores[c2][indexes[c2]] < 0 ? -1 : 1);
-		int maxC = 0;
-		for (int c = 0; c < N; c++) {
-			pq.add(c);
-			if (scores[maxC][indexes[maxC]] < scores[c][indexes[c]]) maxC = c;
-		}
-		while (!pq.isEmpty()) {
-			int minC = pq.poll();
-			result = Math.min(result, Math.abs(scores[minC][indexes[minC]] - scores[maxC][indexes[maxC]]));
-			if (++indexes[minC] == M) break;
-			if (scores[maxC][indexes[maxC]] < scores[minC][indexes[minC]]) maxC = minC;
-			pq.add(minC);
-		}
-	}
+  void solution() {
+    PriorityQueue<Integer> pq = new PriorityQueue<>((c1, c2) -> Long.compare(scores[c1][indexes[c1]], scores[c2][indexes[c2]]));
+    int maxIndex = 0;
+    for (int i = 0; i < N; i++) {
+      pq.add(i);
+      if (scores[maxIndex][indexes[maxIndex]] < scores[i][indexes[i]]) maxIndex = i;
+    }
+    while (!pq.isEmpty()) {
+      int minIndex = pq.poll();
+      result = Math.min(result, scores[maxIndex][indexes[maxIndex]] - scores[minIndex][indexes[minIndex]]);
+      if (++indexes[minIndex] == M) break;
+      if (scores[maxIndex][indexes[maxIndex]] < scores[minIndex][indexes[minIndex]]) maxIndex = minIndex;
+      pq.add(minIndex);
+    }
+  }
 }
